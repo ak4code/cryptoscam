4dafd70dacc0d10006ced136a318b8d87bf6b8ec
axd3v <haritonov.aka@gmail.com>
2024-10-18 20:21:29 +0300

    rebase

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+
+
+bot = telebot.TeleBot("secret_token", parse_mode='html')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Бабки есть?")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	if message != 'Есть':
+		nice_reply_massage = f'{message.from_user.first_name}, жду бабки на карту, beach!'
+		bot.reply_to(message, nice_reply_massage)
+		return
+	bad_reply_massage = f'Бичам тут не место'
+	bot.reply_to(message, bad_reply_massage)
+
+
+if __name__ == "__main__":
+	bot.infinity_polling()
+
